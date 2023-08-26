1f2ba5990dca7f75e3c016afe8021acda3d308b1
Jacob Liddle <jacoboelhobo@gmail.com>
2023-08-26 16:16:22 -0600

    Initial commit

---
+++ MonsterChase/MonsterChase/MonsterChase.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!";
+}
+
+void startGame()
+{
+
+}
+
+void mainLoop()
+{
+
+}
+
+void moveMonsters()
+{
+
+}
+
+void queryPlayer()
+{
+
+}
