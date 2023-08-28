63e8da8d17fa874b7d2f22d36f09d474b4cd25c2
Jacob Liddle <u1070198@utah.edu>
2023-08-28 13:41:58 -0600

    updating cpp to have more of a framework

--- MonsterChase/MonsterChase/MonsterChase.cpp
+++ MonsterChase/MonsterChase/MonsterChase.cpp
@@ -1,28 +1,83 @@
 #include <iostream>
+#include <stdlib.h>
+#include <conio.h>
 
 using namespace std;
 
-int main()
+string playerName;
+int playerPos[2] = { 0, 0 };
+string monsterNames[5];
+int monsterPos[5][2];
+
+void printPos(int pos[2])
 {
-    cout << "Hello World!";
+    cout << "[" << pos[0] << ", " << pos[1] << "]";
+}
+
+void getPlayerName()
+{
+    cout << "Enter Player Name: ";
+    cin >> playerName;
+}
+
+void getNumMonsters()
+{
+
+}
+
+void placeMonsters()
+{
+
 }
 
 void startGame()
+{
+    getPlayerName();
+    getNumMonsters();
+    placeMonsters();
+}
+
+void moveMonsters()
+{
+    //For each monster
+    //Choose x or y, pos or neg, add that to monster position
+}
+
+void addMonsters()
+{
+
+}
+
+void displayMonsters()
+{
+
+}
+
+void displayPlayer()
+{
+    cout << playerName;
+    printPos(playerPos);
+    cout << endl;
+}
+
+void queryPlayer()
 {
 
 }
 
 void mainLoop()
 {
-
+    addMonsters();
+    moveMonsters();
+    displayMonsters();
+    displayPlayer();
+    queryPlayer();
 }
 
-void moveMonsters()
+int main()
 {
+    cout << "Starting Monster Chase" << endl;
 
-}
-
-void queryPlayer()
-{
-
+    startGame();
+    mainLoop();
 }
