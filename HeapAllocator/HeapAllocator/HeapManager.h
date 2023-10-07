991187b1b76742daf4f9e21c158d754b58095a2a
Jacob Liddle <jacoboelhobo@gmail.com>
2023-10-06 22:21:12 -0600

    Somehow lost my .h file, recreating it and making sure it is there this time

---
+++ HeapAllocator/HeapAllocator/HeapManager.h
@@ -0,0 +1,32 @@
+#pragma once
+
+class HeapManager 
+{
+	struct MemoryBlock {
+		void* baseAddress;
+		size_t blockSize;
+		MemoryBlock* nextBlock;
+	};
+
+	MemoryBlock* freeBlocks;
+	MemoryBlock* outstandingBlocks;
+	MemoryBlock* blockList;
+
+	void initMemoryBlocks(void* blocksMemory, size_t blocksMemorySize);
+	void trackAlloc(MemoryBlock* block);
+	MemoryBlock* findFirstFittingFreeBlock(size_t i_size);
+	MemoryBlock* getFreeMemoryBlock();
+	void returnMemoryBlock(MemoryBlock* block);
+	void removeFromFreeList(MemoryBlock* emptyFreeBlock);
+	MemoryBlock* removeOutstandingBlock(void* i_ptr);
+
+
+public:
+	HeapManager(void* heapMemory, size_t heapSize, unsigned int numDescriptors);
+
+	void Collect();
+	void* alloc(size_t i_size, unsigned int alignment = 4);
+	void freeBlock(void* i_ptr);
+	bool contains(void* i_ptr) const;
+	bool isAllocated(void* i_ptr) const;
+};
