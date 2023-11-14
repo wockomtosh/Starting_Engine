850515139163345701fb23846dbc960211915c21
Jacob Liddle <jacoboelhobo@gmail.com>
2023-11-14 16:29:32 -0700

    Getting new code after class

--- ClassDesignIssues/ClassDesignIssues.cpp
+++ ClassDesignIssues/ClassDesignIssues.cpp
@@ -20,6 +20,14 @@ class PlayerActor : public GameActor
 public:
 	PlayerActor(const char* i_pName)
 	{
+		// I have added this ActorNumber counter such that
+		// every instance of PlayerActor created will have a unique
+		// actor number (m_ActorNumber). This may aid you in debugging
+		// the crashes and memory leaks.
+		static int ActorNumber = 0;
+
+		m_ActorNumber = ActorNumber++;
+
 		m_pName = _strdup(i_pName ? i_pName : "Generic");
 	}
 
@@ -31,27 +39,53 @@ public:
 
 	~PlayerActor()
 	{
-		delete(m_pName);
+		free(const_cast<char*>(m_pName));
+	}
+
+	void Update()
+	{
+		printf("Updating %s.", m_pName);
 	}
 
 	const char* getActorType() const { return "Player"; }
 	const char* getName() const { return m_pName; }
-	void Update() {}
 private:
+	int m_ActorNumber;
 	const char* m_pName;
 };
 
+// Function declarations for our test problems
+void Test0();
 void Test1();
 void Test2();
+void Test3();
+
 
 void main(int i_argc, char** i_argl)
 {
+	// Your assignment is to fix all the warnings, crashes and memory leaks
+	// that happen when running this program.
+
+	// Each function may have specific requirements as to how any issue
+	// within need to be resolved.
+
+	Test0();
 	Test1();
 	Test2();
+	Test3();
 
 #if defined _DEBUG
+	// This should not emit any messages about memory leaks.
 	_CrtDumpMemoryLeaks();
 #endif // _DEBUG
+}
+
+void Test0()
+{
+	// This function should work, not crashing or generating any memory leaks
+	PlayerActor Joe("Joe");
+
+	Joe.Update();
 }
 
 void Test1()
@@ -69,28 +103,54 @@ void Test1()
 
 	std::random_shuffle(TestActors.begin(), TestActors.end());
 
-	// pretend we call PlayGame() here.
+	for (auto iter = TestActors.begin(); iter != TestActors.end(); ++iter)
+	{
+		// do something interesting with our PlayerActors
+		iter->Update();
+	}
 }
 
 void Test2()
 {
 	// fix the memory leaks caused by this sample code
+	// by ONLY making changes to this function
+	// No changes can be made to the PlayerActor class
+	// Hint: You probably want to use _CrtDumpMemoryLeaks()
+	// but maybe not inside the function. Why?
+
+	std::vector<PlayerActor*> TestPlayerActorPtrs;
+
+	TestPlayerActorPtrs.push_back(new PlayerActor("Player1"));
+	TestPlayerActorPtrs.push_back(new PlayerActor("Player2"));
+	TestPlayerActorPtrs.push_back(new PlayerActor("Player3"));
+	TestPlayerActorPtrs.push_back(new PlayerActor("Player4"));
+
+	for (auto iter = TestPlayerActorPtrs.begin(); iter != TestPlayerActorPtrs.end(); ++iter)
+	{
+		// do something interesting with our PlayerActors
+		if (*iter)
+			(*iter)->Update();
+	}
+}
+
+void Test3()
+{
+	// fix the memory leaks caused by this sample code
 	// by ONLY making changes to the GameActor class.
 	// No changes can be made to this code
+	// Hint: You probably want to use _CrtDumpMemoryLeaks()
 
-	std::vector<GameActor*> TestActorPtrs;
+	std::vector<GameActor*> TestGameActorPtrs;
 
-	TestActorPtrs.push_back(new PlayerActor("Player1"));
-	TestActorPtrs.push_back(new PlayerActor("Player2"));
-	TestActorPtrs.push_back(new PlayerActor("Player3"));
-	TestActorPtrs.push_back(new PlayerActor("Player4"));
+	TestGameActorPtrs.push_back(new PlayerActor("Player1"));
+	TestGameActorPtrs.push_back(new PlayerActor("Player2"));
+	TestGameActorPtrs.push_back(new PlayerActor("Player3"));
+	TestGameActorPtrs.push_back(new PlayerActor("Player4"));
 
-	// pretend we call PlayGame() here.
-
-	// clean up
-	for (auto iter = TestActorPtrs.begin(); iter != TestActorPtrs.end(); ++iter)
+	for (auto iter = TestGameActorPtrs.begin(); iter != TestGameActorPtrs.end(); ++iter)
 	{
+		// do something interesting with our PlayerActors
 		if (*iter)
-			delete* iter;
+			(*iter)->Update();
 	}
 }
